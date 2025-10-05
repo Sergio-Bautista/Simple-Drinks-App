62546447e76231a7ee57f1ae28363467067ccca8
sergio <69440338+Sergio-Bautista@users.noreply.github.com>
2025-10-04 21:44:56 -0500

    Changed layout and added a new function to generate random drink

--- simple-drinks-app/js/main.js
+++ simple-drinks-app/js/main.js
@@ -1,14 +1,21 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
-
 // set up the variables for the HTML tags 
-const cocktailName = document.querySelector("h2");
+
+const cocktailName = document.querySelector(".cocktailName");
 const cocktailImg = document.querySelector("img");
 const cocktailInstructions = document.querySelector(".instructions");
 const cocktailIngredients = document.querySelector(".ingredients")
 const cocktailMeasure = document.querySelector(".measure");
 const cocktailSuggestions= document.querySelector(".suggestions")
 const cocktailVideo = document.querySelector("p");
+const getCocktail = document.querySelector("#drink").addEventListener("click", getDrink)
+const randomDrink = document.querySelector("#randomDrink").addEventListener("click", getRandomDrink)
+const displayInstructions = document.querySelector('.displayInstructions')
+
+const displayname = document.querySelector(".display")
+
+
 function clear(){
     console.clear()
     cocktailVideo.innerText = ""
@@ -31,16 +38,27 @@ function filterDrinks(arr, userRequest){
     return arr.drinks.filter(drink => drink.strDrink.toLowerCase().includes(userRequest.toLowerCase()))
 }
 
-const getCocktail = document.querySelector("button")
-getCocktail.addEventListener("click", getDrink)
+function getRandomDrink(){
+    clear()
+    const randomDrinkURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    fetch(randomDrinkURL)
+    .then(response => response.json())
+    .then (data =>{
+        // console.log("got it")
+        cocktailImg.src = data.drinks[0].strDrinkThumb;
+        cocktailName.innerText = data.drinks[0].strDrink;
+        displayInstructions.innerText = "Instructions:"
+        cocktailInstructions.innerText = data.drinks[0].strInstructions;
+        propertiesLoop(data.drinks, "strIngredient").forEach((ingredient, index) => cocktailIngredients.innerText += `\nIngredient ${index+1} - ${ingredient}`)
+        propertiesLoop(data.drinks, "strMeasure").forEach((measurement, index) => cocktailMeasure.innerText += `\nMeasurement ${index+1} - ${measurement}`)
+    })
+
+}
 
 // get the response from the server and filter by the name of the user input
 function getDrink(){
     clear()
-
     const userRequest = document.querySelector("input").value.trim().toLowerCase(); // gets user input 
-    // console.log(userRequest)
-    
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userRequest}`)
     .then(response => response.json())
     .then(data => {
@@ -48,14 +66,15 @@ function getDrink(){
         // console.log(name)
         if(name[0].strDrink.toLowerCase() === userRequest.toLowerCase()){
             clear()
-            console.log(name)
-            const ingredientList = propertiesLoop(name, "strIngredient")
-            const measurementList = propertiesLoop(name, "strMeasure")
+            // console.log(name)
             cocktailName.innerText = name[0].strDrink;
-            cocktailInstructions.innerText = name[0].strInstructions;
             cocktailImg.src = name[0].strDrinkThumb;
-            ingredientList.forEach((ingredient, index) => cocktailIngredients.innerText += `\nIngredient ${index+1} - ${ingredient}`)
-            measurementList.forEach((measurement, index) => cocktailMeasure.innerText += `\nMeasurement ${index+1} - ${measurement}`)
+            cocktailInstructions.innerText = name[0].strInstructions;
+            displayInstructions.innerText  = "Instructions:"
+            // displayname.innerText = name[0].strDrink
+            // displayname.style.backgroundImage = `url(${name[0].strDrinkThumb})`
+            propertiesLoop(name, "strIngredient").forEach((ingredient, index) => cocktailIngredients.innerText += `\nIngredient ${index+1} - ${ingredient}`)
+            propertiesLoop(name, "strMeasure").forEach((measurement, index) => cocktailMeasure.innerText += `\nMeasurement ${index+1} - ${measurement}`)
     
             if(name.length > 1){
                 cocktailSuggestions.innerText = "Similar Drinks you might enjoy: " 
@@ -73,12 +92,7 @@ function getDrink(){
                 cocktailSuggestions.innerText += `\n${index+1} - ${name.strDrink}`
             });
         }
-
-
-
-
     })
     .catch(err => console.log(err))
-   
 }
 
